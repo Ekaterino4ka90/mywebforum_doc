84e1aef9d51b376b32fa4bd1f49a169bfa7a84b0
Ekaterino4ka <ebudoragina@gmail.com>
2023-04-12 00:09:25 -0700

    Update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,6 +38,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: "/",
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
@@ -46,6 +47,7 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        blog: false,
       }),
     ],
   ],
