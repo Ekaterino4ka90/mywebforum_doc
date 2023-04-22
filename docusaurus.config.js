8d51d7484a67b31f0b6d0d9db626ce41360e008a
Ekaterino4ka <ebudoragina@gmail.com>
2023-04-22 09:19:25 -0700

    Updated Github URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,7 +42,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/Ekaterino4ka90/mywebforum_doc",
+          editUrl: "https://github.com/Ekaterino4ka90/mywebforum_doc/tree/main",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
